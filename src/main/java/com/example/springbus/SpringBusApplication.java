be6f55bef47de6c1a328fb663819dc3f815f26ae
juxin <wisebond@sohu.com>
2018-11-20 07:43:59 +0800

    Spring cloud bus demo for learning not business.

---
+++ src/main/java/com/example/springbus/SpringBusApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBusApplication.class, args);
+    }
+}
